d063869817a37259d2a92c0d9992e7e4f9d2a200
clairestanleystl <62560861+clairestanleystl@users.noreply.github.com>
2021-03-17 16:45:20 -0500

    completed

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+       response.json().then( function(json) {
+          console.log(json);
+          
+          const div = document.getElementById("container")
+
+          for (let i = 0; i < json.length; i++) {
+              
+            let card = document.createElement("div");
+            card.innerHTML = `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName + " " + json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                </div>         
+              `
+
+            div.appendChild(card)
+
+          }
+
+
+
+       });
+    });
+ });
